18581f0d266a58719375480a556126665d12f898
nostopgmaming17 <65682727+nostopgmaming17@users.noreply.github.com>
2020-11-24 10:09:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const Discord = require("discord.js");
 
 const client = Discord.Client();
 
+client.on("ready", () => {
+    console.log("${client.user.username} Is online!")
+})
+
 client.login(process.env.token);
