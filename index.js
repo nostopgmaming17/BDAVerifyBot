83be03f23bf8f0ef0ffbe8b85b3864d5fdc0d485
nostopgmaming17 <65682727+nostopgmaming17@users.noreply.github.com>
2020-11-24 10:37:56 +0200

    First

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log("${client.user.username} Is online!")
+    console.log(client.user.username + " Is online!")
 })
 
+console.log(process.env.token);
 client.login(process.env.token);
