9946a7f17ab9cec556218653d0d4f6e5e57b90a4
nostopgmaming17 <65682727+nostopgmaming17@users.noreply.github.com>
2020-11-24 10:46:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log(client.user.username + " Is online!")
-})
+});
 
-
+client.on("message", msg => {
+    if (msg.author.bot) return;
+    if (msg.content.toLowerCase() == "ping")
+    {
+        msg.reply("pong");
+    };
+});
 client.login(process.env.token);
